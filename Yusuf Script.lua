e2b26140ac1e89c798e37d46dd4089546a5cedf3
kfdjdk72 <morteuxontop@gmail.com>
2026-09-20 19:39:53 +0300

    Update Yusuf Script.lua

--- Yusuf Script.lua
+++ Yusuf Script.lua
@@ -248,9 +248,7 @@ local function fastRebirthLoop()
         equipPets({
             ["Swift Samurai"] = 4,
             ["Omega Overlord"] = 1,
-            ["Legendary Boss Pet"] = 1,
-            ["Powercore Hound"] = 1,
-            ["Titanium Hydra"] = 1
+            ["Rare Boss Pet"] = 3
         })
         doRebirth()
         task.wait(0.5)
